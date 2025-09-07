45d90b141fcbee3d6c5930cfefadd35045bdb316
4xcashback-ai <4xcashback@gmail.com>
2025-09-07 01:40:07 +0000

    auto-commit for 3b34d388-576d-4537-b44a-7b29a55159e3

--- frontend-php/index.php
+++ frontend-php/index.php
@@ -1,14 +1,17 @@
 <?php
 session_start();
 
+// Debug info
+$is_logged_in = isset($_SESSION['user_id']) && isset($_SESSION['access_token']);
+
 // Check if user is logged in
-if (isset($_SESSION['user_id']) && isset($_SESSION['access_token'])) {
+if ($is_logged_in) {
     // User is logged in, redirect to search page
     header('Location: search.php');
     exit;
 } else {
     // User is not logged in, show landing page
-    include 'landing.php';
+    require_once 'landing.php';
     exit;
 }
 ?>
